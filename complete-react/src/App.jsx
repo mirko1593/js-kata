bab9e22a19b0acfa93bb8f562e832b1fb469b59d
mirko1593 <mirko1593@hotmail.com>
2023-03-20 15:39:49 +0800

    react router dom

--- complete-react/src/App.jsx
+++ complete-react/src/App.jsx
@@ -1,19 +1,26 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path={`/details/:id`} element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
 const root = createRoot(document.getElementById("root"));
 
-root.render(React.createElement(App));
+root.render(<App />);
 
 // return React.createElement("div", {}, [
 //   React.createElement("h1", {}, "Adopt Me!"),
