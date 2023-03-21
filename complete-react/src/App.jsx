6646bbf3af113fcf1fe8a08420ef9748fb52d228
mirko1593 <mirko1593@hotmail.com>
2023-03-21 19:22:40 +0800

    add code splitting and Suspense

--- complete-react/src/App.jsx
+++ complete-react/src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import SearchParams from "./SearchParams";
-import Details from "./Details";
 import AdoptedPetContext from "./AdoptedPetContext";
+
+const Details = lazy(() => import("./Details"));
+const SearchParams = lazy(() => import("./SearchParams"));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,13 +23,21 @@ const App = () => {
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AdoptedPetContext.Provider value={adoptedPet}>
-          <header>
-            <Link to="/">Adopt Me!</Link>
-          </header>
-          <Routes>
-            <Route path={`/details/:id`} element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
+          <Suspense
+            fallback={
+              <div className="loading-pane">
+                <h2 className="loader">...</h2>
+              </div>
+            }
+          >
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
+              <Route path={`/details/:id`} element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Suspense>
         </AdoptedPetContext.Provider>
       </QueryClientProvider>
     </BrowserRouter>
